b9bef9a45a117d276b8abe13ef260c3098db157f
Aditya Nanda Tri Prakoso <adityanandaaa@gmail.com>
2020-03-07 17:12:45 +0700

    initial home data engineer

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
+import HomeDataEng from "./containers/DataEngineer/HomeDataEng/HomeDataEng";
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="dataenghome" component={HomeDataEng}/>
     </Switch>
+
 );
 
 export default Routes;
