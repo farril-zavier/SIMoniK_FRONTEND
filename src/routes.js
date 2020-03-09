8662dbca238925856ffa882b8a66272ffe2ac731
Aditya Nanda Tri Prakoso <adityanandaaa@gmail.com>
2020-03-09 15:08:18 +0700

    rapihin routes.js

--- src/routes.js
+++ src/routes.js
@@ -10,10 +10,13 @@ import DetailProduk from "./containers/DataEngineer/Produk/DetailProduk";
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Home} />
+
+        {/* DataEngineer */}
         <Route exact path="/stock" component={StockOpname} />
         <Route exact path="/produk" component={Produk} />
         <Route exact path="/produk/tambah" component={TambahProduk} />
         <Route exact path="/produk/detail" component={DetailProduk} />
+
         <Route exact path="/daftarAkun" component={DaftarAkun} />
     </Switch>
 );
