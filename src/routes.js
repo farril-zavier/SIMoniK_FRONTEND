e31780e45a2a6f5eeff7005a85a6cafc56b7a1fe
Aditya Nanda Tri Prakoso <adityanandaaa@gmail.com>
2020-03-09 14:05:43 +0700

    tambah produk page & tambah produk page

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,17 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import StockOpname from "./containers/DataEngineer/StockOpname/StockOpname";
 import DaftarAkun from "./containers/Admin/DaftarAkun/DaftarAkun";
+import Produk from "./containers/DataEngineer/Produk/Produk";
+import TambahProduk from "./containers/DataEngineer/Produk/TambahProduk";
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/stock" component={StockOpname}/>
+        <Route exact path="/stock" component={StockOpname} />
+        <Route exact path="/produk" component={Produk} />
+        <Route exact path="/produk/tambah" component={TambahProduk} />
         <Route exact path="/daftarAkun" component={DaftarAkun} />
     </Switch>
-
 );
 
 export default Routes;
